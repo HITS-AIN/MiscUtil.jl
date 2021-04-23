3b449ad15011c8f9a4106608bef0ca444ebeb8f3
ngiann <ngiann@gmail.com>
2021-04-23 17:01:59 +0200

    export functions

--- src/MiscUtil.jl
+++ src/MiscUtil.jl
@@ -1,5 +1,7 @@
 module MiscUtil
 
-# Write your package code here.
+export calculaterbfbasis, calculaterbfbasis!, safewrapper, addjitter, addjitter!, makematrixsymmetric, makematrixsymmetric!, woodbury_A_plus_UCV
+
+include("util.jl")
 
 end
