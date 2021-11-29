df71348fc7bd448375fa53e765f07be0e5146e3e
ngiann <ngiann@gmail.com>
2021-11-29 09:33:37 +0100

    added tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,29 @@ using MiscUtil
 using Test
 
 @testset "MiscUtil.jl" begin
-    # Write your tests here.
+
+    ###########
+    # sigmoid #
+    ###########
+
+     for x in [1e-6;0.0;1.0;5.0]
+       @test abs(invsigmoid(sigmoid(x)) - x) < 1e-8
+     end
+
+    ####################
+    # transformbetween #
+    ####################
+
+    for x in [1e-6;0.0;1.0;5.0]
+        @test abs(invtransformbetween(transformbetween(x, -1.3, 10.1), -1.3, 10.1) - x) < 1e-8
+    end
+
+    ################
+    # makepositive #
+    ################
+
+    for x in [1e-3;0.0;1.0;5.0]
+        @test abs(invmakepositive(makepositive(x)) - x) < 1e-8
+    end
+
 end
