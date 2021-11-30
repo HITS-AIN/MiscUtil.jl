d25d91438b484ba89655d7a624a701265902b117
Nikos Gianniotis <ngiann@gmail.com>
2021-11-30 16:24:03 +0100

    Update sleeputil.jl

--- src/sleeputil.jl
+++ src/sleeputil.jl
@@ -11,7 +11,7 @@ function tasksleep(;id=id::Int, workfor::Int=workfor, sleepfor::Int=sleepfor)
         run(resumecmd)
 
         for i in 0:workfor-1
-            local msg = @sprintf("\rProcess %d is working for %d seconds  😴", id, workfor-i)
+            local msg = @sprintf("\rProcess %d is working for %d seconds 🙂", id, workfor-i)
             print(Crayon(foreground = :green,  bold = true), @sprintf("%s",msg), Crayon(reset = true))
             sleep(1)
         end
@@ -20,7 +20,7 @@ function tasksleep(;id=id::Int, workfor::Int=workfor, sleepfor::Int=sleepfor)
         run(stopcmd)
 
         for i in 0:sleepfor-1
-            local msg = @sprintf("\rProcess %d is sleeping for %d seconds 🙂", id, sleepfor-i)
+            local msg = @sprintf("\rProcess %d is sleeping for %d seconds 😴", id, sleepfor-i)
             print(Crayon(foreground = :light_red,  bold = true), @sprintf("%s",msg), Crayon(reset = true))
             sleep(1)
 
